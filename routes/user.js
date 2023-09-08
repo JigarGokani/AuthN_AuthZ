8534f1c2d2653534b602c45c060760201f4a9d13
Jigar-gokani <jigar.gokani@somaiya.edu>
2023-09-08 09:32:34 +0530

    protected route

--- routes/user.js
+++ routes/user.js
@@ -3,9 +3,26 @@ const router= express.Router();
 
 
 const{login,signup} = require("../Controllers/Auth");
+const{auth,isStudent,isAdmin} = require("../middlewares/auth");
 
 router.post("/login",login);
 router.post("/signup",signup);
 
+
+// Protected Routes
+router.get("/student",auth,isStudent,(req,res) => {
+    res.json({
+        success:true,
+        message:"Welcome to the Protected route for Students",
+    });
+});
+
+
+router.get("/admin",auth,isAdmin,(req,res) => {
+    res.json({
+        success:true,
+        message:"Welcome to the Protected route for Admin",
+    });
+});
 
 module.exports = router;
